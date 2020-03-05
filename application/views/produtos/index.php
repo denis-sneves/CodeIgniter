a0e0c28fe3e66028ee3052ecb4790db20b061337
Denis Neves <denis.sneves@outlook.com>
2020-03-04 23:27:42 -0300

    Projeto Completo

--- application/views/produtos/index.php
+++ application/views/produtos/index.php
@@ -151,7 +151,7 @@ body{
  <section id="footer">
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-        p>Sistema de Estoque</p>
+					<p>Sistema de Estoque</p>
 					<p class="h6">&copy Denis Neves.</p>
             </div>
 			</div>	
